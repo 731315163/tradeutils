d89200697bd7b131fe0a6aef241c1fd51506dcdc
zt <731315163@qq.com>
2025-08-05 14:05:28 +0000

    0.1.1

--- tradeutils/technical_analysis/__init__.py
+++ tradeutils/technical_analysis/__init__.py
@@ -1,5 +1,5 @@
 from .direction import linear_regression
-from .volume_indicator import VWAP
+from .volume_indicator import VWAP,filter_volume
 from .Indicator import (get_corrcoef,
 calculate_timing_indicator,calculate_trend_score,calculate_emotion_index,
 calculate_anchored_trend,caculate_stochastic_signals,
@@ -8,10 +8,13 @@ calculate_adjusted_rsi_oscillators,caculate_williams_signals,roc_periods,
 hurst_exponent,sma_crossover_periods,sma_periods,
 linear_regression_periods,average_arrays_strict_nan)
 
-
+from volatility_indicators import PTR,PTRP
 
 __all__ = [
-    "volume_indicator",
+    "PTR",
+    "PTRP",
+    "VWAP",
+    "filter_volume",
     "linear_regression",
     "get_corrcoef",
     "calculate_timing_indicator",
